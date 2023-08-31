68104c8360b4075568c95e72674dc3132939e787
Emerson <emersonmedina42@gmail.com>
2023-08-31 15:49:37 -0300

    PUERTOS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, console.log("Starting server on port " + port));
 connectDB();
